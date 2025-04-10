bfd7eeb639ea8640c82dad531626447718b331b5
Joalor64 <rhgarcon@gmail.com>
2025-04-10 16:30:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+    socket.on('send name', (username) => {
+        io.emit('send name', (username));
+    });
+
+    socket.on('send message', (chat) => {
+        io.emit('send message', (chat));
+    });
+});
+
 server.listen(port, () => {
     console.log(`Server is listening at the port: ${port}`);
 });
