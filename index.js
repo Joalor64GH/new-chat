3b0b2bda6ead296971ddbd1086e32adcb342ad9d
Joalor64 <rhgarcon@gmail.com>
2025-04-10 16:30:25 +0000

    fix again idk

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    socket.on('send name', (username) => {
-        io.emit('send name', (username));
-    });
-
-    socket.on('send message', (chat) => {
-        io.emit('send message', (chat));
-    });
-});
-
 server.listen(port, () => {
     console.log(`Server is listening at the port: ${port}`);
 });
