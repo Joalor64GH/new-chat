c7c2056e9c31cb8a93d9359e3987c28b1cd1300b
Joalor64 <rhgarcon@gmail.com>
2025-04-10 14:41:24 +0000

    revert again idk

--- index.js
+++ index.js
@@ -8,26 +8,17 @@ const server = http.createServer(app);
 const io = new Server(server);
 const port = 5000;
 
-const onlineUsers = {};
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
     socket.on('send name', (username) => {
-        onlineUsers[socket.id] = data.username;
-        io.emit("update users", Object.values(onlineUsers));
         io.emit('send name', (username));
     });
 
     socket.on('send message', (chat) => {
         io.emit('send message', (chat));
-    });
-
-    socket.on("disconnect", () => {
-        delete onlineUsers[socket.id];
-        io.emit("update users", Object.values(onlineUsers)); 
     });
 });
 
